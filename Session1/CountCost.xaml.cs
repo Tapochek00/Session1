5d8a2dbc5fc14fc2a6da472ac203a3b0986953f9
ИСП-31 <-31@OMEGA007>
2023-02-08 10:28:33 +0300

    seye

--- Session1/CountCost.xaml.cs
+++ Session1/CountCost.xaml.cs
@@ -29,10 +29,14 @@ namespace Session1
         {
             Deals deal = db.Deals.Find(Data.Id);
             supplies sup = db.supplies.Find(deal.SupplyId);
+            Demands dem = db.Demands.Find(deal.DemandId);
             string type = db.RealEstateObjects.Find(sup.RealEstateId).ObjectType;
-            double dealShare = (double)db.Realtor.Find(sup.AgentId).DealShare;
+            double dealShareSell = (double)db.Realtor.Find(sup.AgentId).DealShare;
+            double dealShareBuy = (double)db.Realtor.Find(dem.AgentId).DealShare;
             double price = sup.Price;
-            double result;
+            double result = 0;
+            double buy;
+            double sell;
             if (combo.Text.Length == 0) MessageBox.Show("Выберите, для кого нужно произвести расчёт");
             else if (combo.Text == "Клиент-продавец")
             {
@@ -51,25 +55,27 @@ namespace Session1
                 if (type == "Квартира") result = 36000 + price * 0.01;
                 else if (type == "Дом") result = 30000 + price * 0.01;
                 else result = 30000 + price * 0.02;
-                if (dealShare != 0) result *= dealShare / 100;
+                if (dealShareSell != 0) result *= dealShareSell / 100;
                 else result *= 0.45;
                 MessageBox.Show($"Размер отчислений риэлтору клиента-продавца: {result}");
             }
             else if (combo.Text == "Риэлтор клиента-покупателя") 
             {
                 result = price * 0.03;
-                if (dealShare != 0) result *= dealShare / 100;
+                if (dealShareBuy != 0) result *= dealShareBuy / 100;
                 else result *= 0.45;
                 MessageBox.Show($"Стоимость услуг для клиента-покупателя: {result}");
             }
             else if (combo.Text == "Размер отчислений компании") 
             {
-                if (type == "Квартира") result = 36000 + price * 0.01;
-                else if (type == "Дом") result = 30000 + price * 0.01;
-                else result = 30000 + price * 0.02;
-                result += price * 0.03;
-                if (dealShare != 0) result *= 1 - dealShare / 100;
-                else result *= 0.55;
+                if (type == "Квартира") sell = 36000 + price * 0.01;
+                else if (type == "Дом") sell = 30000 + price * 0.01;
+                else sell = 30000 + price * 0.02;
+                buy = price * 0.03;
+                if (dealShareSell != 0) result += sell * (1 - dealShareSell / 100);
+                else result += sell * 0.55;
+                if (dealShareBuy != 0) result += buy * (1 - dealShareBuy / 100);
+                else result += buy * 0.55;
                 MessageBox.Show($"Размер отчислений компании: {result}");
             }
         }
